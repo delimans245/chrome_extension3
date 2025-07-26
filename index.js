795747462a7268040cc46fea091637fd1701feb3
Ifaz Howlader <ifaz.howl@gmail.com>
2025-07-25 23:18:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let links = []
+let saveBtn = document.getElementById('saveButton');
+let saveTab = document.getElementById('saveTabButton');
+let deleteBtn = document.getElementById('deleteButton');
+
+saveBtn.addEventListener('click', function () {
+    
+})
